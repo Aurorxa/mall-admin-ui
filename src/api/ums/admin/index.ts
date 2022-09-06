a3de574164eb3b3973f226d657c89efdc66e530b
许大仙 <1900919313@qq.com>
2022-09-06 16:22:25 +0800

    🎨: 登录功能

--- src/api/ums/admin/index.ts
+++ src/api/ums/admin/index.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import {LoginFormType, LoginReturnType} from "@/types/ums/admin"
 
 /* 登录 */
-export function login(data: LoginFormType): Promise<LoginReturnType> {
+export const login: (data: LoginFormType) => Promise<LoginReturnType> = data => {
     return request.post('/ums/doLogin', data)
 }
 
