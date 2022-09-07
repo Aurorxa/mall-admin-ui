3f1a87b89e34bc96b0c01e2bd549cd6dfb9ef677
许大仙 <1900919313@qq.com>
2022-09-07 14:04:34 +0800

    🎨: 修改密码前端验证逻辑

--- src/api/ums/admin/index.ts
+++ src/api/ums/admin/index.ts
@@ -3,11 +3,11 @@ import {LoginFormType, LoginReturnType} from "@/types/ums/admin"
 
 /* 登录 */
 export const login: (data: LoginFormType) => Promise<LoginReturnType> = data => {
-    return request.post('/ums/doLogin', data)
+    return request.post('/ums/admin/doLogin', data)
 }
 
 
 export const test = () => {
-    return request.get('/ums/test')
+    return request.get('/ums/admin/test')
 }
 
