c47677870e01a4bcf2a62a7df9b3dcab43b080d7
许大仙 <1900919313@qq.com>
2022-09-09 17:02:16 +0800

    🎨: 国际化缓存处理问题

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -16,22 +16,6 @@ const messages = {
     }
 }
 
-
-function checkIsJsonString(str: string) {
-    try {
-        //通过JSON将str转换为json对象，如果转换出现异常，进入catch,返回false
-        let obj = JSON.parse(str);
-        if (typeof obj === 'object' && obj) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        //转换异常，返回false
-        return false;
-    }
-}
-
 const i18n = createI18n({
     legacy: false, // 使用 Composition API
     globalInjection: true, // 全局使用 t 函数
