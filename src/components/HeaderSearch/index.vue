4355a610a0e44a082e06bfd698641ee8c1282a62
许大仙 <1900919313@qq.com>
2022-09-10 22:41:12 +0800

    🎨: HeaderSearch组件开发

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -25,31 +25,159 @@
 <script lang="ts" setup>
 // 控制 Search 展示
 import {ElSelect} from "element-plus"
+// 模糊搜索
+import Fuse from 'fuse.js'
 
 const isShow = ref<boolean>(false)
 
 // 搜索的内容
 const search = ref<string>('')
-const loading = ref<boolean>(false)
 // el-select 实例
 const headerSearchSelectRef = ref<typeof ElSelect>()
 const onShowClick = () => {
   isShow.value = !isShow.value
-  if(headerSearchSelectRef.value) {
+  if (headerSearchSelectRef.value) {
     headerSearchSelectRef.value.focus()
   }
 }
-
-const options = [
+// 检索数据源
+type MenuDataType = {
+  id: string,
+  title: string,
+  icon: string,
+  path: string,
+  children?: MenuDataType[]
+}
+const menuList: MenuDataType[] = [
   {
-    value: 'Option1',
-    label: 'Option1',
+    id: '1',
+    title: '首页',
+    icon: 'i-ep-home-filled',
+    path: '/',
+    children: []
+  },
+  {
+    id: '2',
+    title: '系统设置',
+    icon: 'i-ep-menu',
+    path: '',
+    children: [
+      {
+        id: '3',
+        title: '用户管理',
+        icon: 'i-ep-user',
+        path: '/system/user',
+      },
+      {
+        id: '6',
+        title: '角色管理',
+        icon: 'i-ep-user',
+        path: '/system/role'
+      },
+      {
+        id: '7',
+        title: '菜单管理',
+        icon: 'i-ep-user',
+        path: '/menu'
+      },
+    ]
+  },
+  {
+    id: '8',
+    title: '系统设置1',
+    icon: 'i-ep-menu',
+    path: '',
+    children: [
+      {
+        id: '9',
+        title: '用户管理1',
+        icon: 'i-ep-user',
+        path: '',
+        children: [
+          {
+            id: '10',
+            title: '用户管理11',
+            icon: 'i-ep-user',
+            path: '/admin11',
+          },
+          {
+            id: '11',
+            title: '用户管理21',
+            icon: 'i-ep-user',
+            path: '/admin21',
+          }
+        ]
+      },
+      {
+        id: '12',
+        title: '角色管理1',
+        icon: 'i-ep-user',
+        path: '/role1'
+      },
+      {
+        id: '13',
+        title: '菜单管理1',
+        icon: 'i-ep-user',
+        path: '/menu1'
+      },
+    ]
   }
 ]
 
+type FuseDataType = {
+  title: string | string[],
+  path: string
+}
+
+const handleMenuList = (menuList: MenuDataType[]) => {
+  // 创建 result 数据
+  let result: FuseDataType[] = []
+  if (!Array.isArray(menuList) || menuList.length === 0) {
+    return result;
+  }
+  for (let menu of menuList) {
+    if (menu.children && menu.children.length > 0) {
+      let parentTitle = menu.title;
+      const temp: FuseDataType[] = handleMenuList(menu.children)
+      temp.map(t => t.title = parentTitle + " -> " + t.title)
+      if (temp.length > 0) {
+        result = [...result, ...temp]
+      }
+    } else {
+      // 创建包含 path 和 title 的 item
+      const data: FuseDataType = {
+        path: menu.path,
+        title: menu.title
+      }
+      result.push(data)
+    }
+  }
+  return result;
+}
+
+// 搜索库相关
+const fuse = new Fuse(handleMenuList(menuList), {
+  shouldSort: true, // 是否按照优先级进行排序
+  minMatchCharLength: 1, // 匹配长度超过某个值的被认为是匹配的
+  keys: [ // 将被搜索的键列表，支持嵌套路劲、加权搜索、在字符串和对象数组中搜索
+    {
+      name: 'title', // 搜索的键
+      weight: 0.7 // 权重
+    }, {
+      name: 'path', // 搜索的键
+      weight: 0.3 // 权重
+    }
+  ]
+})
+
 // 搜索的方法
 const querySearch = (query: string) => {
-  console.log('@@@')
+  if (query !== '') {
+    const result = fuse.search(query)
+    console.log('@@@@result', result)
+  }else{
+     // 没有搜索数据
+  }
 }
 
 // 选中回调
