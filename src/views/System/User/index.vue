543b47e23b1441020c932dd0cd40322c61c909e4
许大仙 <1900919313@qq.com>
2022-09-13 16:31:42 +0800

    🎨: 用户管理

--- src/views/System/User/index.vue
+++ src/views/System/User/index.vue
@@ -1,11 +1,141 @@
 <template>
-  用户管理
+  <!-- 搜索 -->
+  <el-row :gutter="10">
+    <el-col :span="4">
+      <el-form-item label="用户名">
+        <el-input />
+      </el-form-item>
+    </el-col>
+    <el-col :span="4">
+      <el-form-item label="邮箱">
+        <el-input />
+      </el-form-item>
+    </el-col>
+    <el-col :span="4">
+      <el-form-item label="手机号码">
+        <el-input />
+      </el-form-item>
+    </el-col>
+    <el-col :span="4">
+      <el-form-item label="状态">
+        <el-select>
+          <el-option label="禁止" value="0" />
+          <el-option label="启用" value="1" />
+        </el-select>
+      </el-form-item>
+    </el-col>
+    <el-col :span="1">
+      <el-button type="primary">
+        <el-icon class="i-ep-search"></el-icon>
+        搜索
+      </el-button>
+    </el-col>
+    <el-col :span="1" style="margin-left: 15px">
+      <el-button type="primary">
+        <el-icon class="i-ep-refresh"></el-icon>
+        重置
+      </el-button>
+    </el-col>
+  </el-row>
+  <!-- 表格 -->
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column label="Date" width="180">
+      <template #default="scope">
+        <div style="display: flex; align-items: center">
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="Name" width="180">
+      <template #default="scope">
+        <el-popover effect="light" trigger="hover" placement="top" width="auto">
+          <template #default>
+            <div>name: {{ scope.row.name }}</div>
+            <div>address: {{ scope.row.address }}</div>
+          </template>
+          <template #reference>
+            <el-tag>{{ scope.row.name }}</el-tag>
+          </template>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column label="Operations">
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
+          编辑
+        </el-button>
+        <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">
+          删除
+        </el-button>
+        <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">
+          删除
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <!-- 分页组件 -->
 </template>
 
 <script setup lang="ts">
 
+interface User {
+  date: string
+  name: string
+  address: string
+}
+
+const handleEdit = (index: number, row: User) => {
+  console.log(index, row)
+}
+const handleDelete = (index: number, row: User) => {
+  console.log(index, row)
+}
+
+const tableData: User[] = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
 
 <style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+}
 
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
 </style>
